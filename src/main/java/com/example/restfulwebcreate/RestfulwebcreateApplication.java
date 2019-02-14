e35a772f372d2bd30361e9788c0b1e6e2eebb909
Darbetigh <mustafaakgul@yandex.com>
2019-02-14 18:22:14 +0300

    Initial commit

---
+++ src/main/java/com/example/restfulwebcreate/RestfulwebcreateApplication.java
@@ -0,0 +1,14 @@
+package com.example.restfulwebcreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulwebcreateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulwebcreateApplication.class, args);
+    }
+
+}
+
